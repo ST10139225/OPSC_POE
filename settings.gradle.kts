082afb0899bdbea9cd2a85522298ed365608dbf8
Koki B <st10139225@rcconnect.edu.za>
2024-06-04 19:37:07 +0000

    New App for login

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Cele_n_Baholo_POE"
+include(":app")
+ 
