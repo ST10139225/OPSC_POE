55dfb9a2e84e0c971460c87e56164abeee74b7d4
Koki B <st10139225@rcconnect.edu.za>
2024-06-04 21:35:24 +0000

    Added the code-behind for the main

--- app/src/main/java/com/example/cele_n_baholo_poe/MainActivity.kt
+++ app/src/main/java/com/example/cele_n_baholo_poe/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.cele_n_baholo_poe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cele_n_baholo_poe.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth :FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        auth = FirebaseAuth.getInstance()
+         binding.BtnLogout.setOnClickListener{
+             Firebase.auth.signOut()
+
+             binding.HomeMSg.text ="Praise The LORD"
+         }
+
+
+
+
     }
 }
