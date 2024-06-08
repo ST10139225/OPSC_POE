b9b2c16f5b5676f53cb37edac65450baed6a6303
Koki B <st10139225@rcconnect.edu.za>
2024-06-08 20:58:02 +0000

    Successfully added a realtime database

--- app/src/main/java/com/example/cele_n_baholo_poe/MainActivity.kt
+++ app/src/main/java/com/example/cele_n_baholo_poe/MainActivity.kt
@@ -3,19 +3,29 @@ package com.example.cele_n_baholo_poe
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.*
 import com.example.cele_n_baholo_poe.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth :FirebaseAuth
+
+    private lateinit var fireRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+
+        fireRef = Firebase.database("https://nk-inventory-26241-default-rtdb.europe-west1.firebasedatabase.app/").getReference("test")
+
 
 
         val user = Firebase.auth.currentUser
@@ -24,6 +34,12 @@ class MainActivity : AppCompatActivity() {
             val name = it.displayName
             val email = it.email
             val photoUrl = it.photoUrl
+
+
+
+
+
+
 
             // Check if user's email is verified
             val emailVerified = it.isEmailVerified
@@ -34,13 +50,24 @@ class MainActivity : AppCompatActivity() {
             val uid = it.uid
         }
         auth = FirebaseAuth.getInstance()
-         binding.BtnLogout.setOnClickListener{
+
+        binding.BtnLogout.setOnClickListener{
              Firebase.auth.signOut()
              finish()
          }
 
 
+        binding.btnCategoryHome.setOnClickListener{
 
+            fireRef.setValue("Praise Thee Lord").addOnCompleteListener{
+                makeToasts("Added to test")
+            }
+        }
+
+    }
+
+    public fun makeToasts(text: String){
+        Toast.makeText(this,text, Toast.LENGTH_LONG).show()
 
     }
 }
