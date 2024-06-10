0229e3a2256a67404b7f2ba2fbeabe49979e290f
Koki B <st10139225@rcconnect.edu.za>
2024-06-10 08:38:02 +0000

    Added the firebase functionality (testing) to Add Item Feature.

--- app/src/main/java/com/example/cele_n_baholo_poe/ItemFeature/AddItem.kt
+++ app/src/main/java/com/example/cele_n_baholo_poe/ItemFeature/AddItem.kt
@@ -2,12 +2,87 @@ package com.example.cele_n_baholo_poe.ItemFeature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.Toast
 import com.example.cele_n_baholo_poe.R
+import com.example.cele_n_baholo_poe.databinding.ActivityAddItemBinding
+import com.example.cele_n_baholo_poe.models.Categorys
+import com.example.cele_n_baholo_poe.models.ItemModel
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.util.Calendar
 
 class AddItem : AppCompatActivity() {
+
+    private lateinit var fireRef: DatabaseReference
+
+
+    private lateinit var binding: ActivityAddItemBinding
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        binding = ActivityAddItemBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_item)
+        setContentView(binding.root)
+
+        var acquisitionDate: Calendar
+        var dateFrmCalView: String =""
+
+        binding.aqDate.setOnDateChangeListener{ calView: CalendarView, year: Int, month: Int, dayOfMonth: Int ->
+            acquisitionDate =  Calendar.getInstance()
+            acquisitionDate.set(year, month, dayOfMonth)
+            calView.setDate(acquisitionDate.timeInMillis, true, true)
+            dateFrmCalView = "$dayOfMonth/${month + 1}/$year"
+
+        }
+
+
+        fireRef = Firebase.database("https://nk-inventory-26241-default-rtdb.europe-west1.firebasedatabase.app/")
+            .getReference("test")
+
+        binding.btnAdd.setOnClickListener{
+            AddItems(dateFrmCalView)
+        }
+    }
+
+    private fun AddItems( AqcDate : String){
+
+        val Name = binding.edtTitle.text.toString()
+        val Description  = binding.edtDescription.text.toString()
+       var dateFrmCalView: String = AqcDate
+
+
+
+
+        if(Name.isNullOrEmpty()||Name.isBlank()){
+            binding.edtTitle.error ="Enter a name"
+
+        }
+        if(Description.isNullOrEmpty()||Description.isBlank()){
+            binding.edtDescription.error ="Enter a description"
+
+        }
+
+        if(dateFrmCalView.isNullOrEmpty()||dateFrmCalView.isBlank()){
+             binding.calLabel.error = "Please select a date !"
+
+        }
+
+        val ItemId = fireRef.push().key!!
+
+         val New_Item =  ItemModel(ItemId,Name,Description,dateFrmCalView)
+
+        fireRef.child(ItemId).setValue(New_Item)
+            .addOnCompleteListener{
+                makeToasts("Glory to Christ The Living LORD")
+            }
+            .addOnFailureListener{
+                makeToasts("Some failure ${it.message}")
+            }
+    }
+
+    fun makeToasts(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+
     }
 }
---
+++ app/src/main/java/com/example/cele_n_baholo_poe/models/ItemModel.kt
@@ -0,0 +1,8 @@
+package com.example.cele_n_baholo_poe.models
+
+data class ItemModel(
+    val id : String? =null,
+    val Name: String? =null,
+    val Description: String?  =null,
+    val Acquisition_Date: String? =null
+)
